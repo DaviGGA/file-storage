0c4a2913fe8e3d933086d2199569da4c179520b4
Davi Gregório <davigregorio.a@hotmail.com>
2025-02-18 14:19:00 -0300

    validator to assure path  and name integrity

--- src/modules/storage/validators/create-folder-validator.ts
+++ src/modules/storage/validators/create-folder-validator.ts
@@ -2,10 +2,21 @@ import { z } from "zod";
 import { CreateFolder } from "../@types/CreateFolder";
 
 const createFolderSchema = z.object({
-  name: z.string().min(1,"field name is required"),
-  path: z.string().nullable()
+  name: z
+    .string()
+    .min(1,"field name is required")
+    .refine(name => 
+      !/[!@#$%^&*(),.?":{}|<>\\/]/.test(name),
+      'The only special character allowed is "-"'
+    ),
+  path: z
+    .string()
+    .nullable()
+    .refine(path => 
+      path ? !/[!@#$%^&*(),.?":{}|<>\\]/.test(path) : true,
+      'The only special characters allowed is "-" or "/"'
+    )
 })
-
 
 export function validateCreateFolder(folder: unknown): CreateFolder  {
   return createFolderSchema.parse(folder);
