dc9b2c37ad14f40526d30cdd9aa292ae9fe87f75
DaviGGA <davigregorio.a@hotmail.com>
2025-02-21 16:26:43 -0300

    remove dropDatabase from index

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,7 @@ import { app } from "./app";
 import { config } from "./config/config";
 import { connectDatabase } from "./database/mongo";
 
-
-
 app.listen(config.PORT, async () => {
   console.log(`Server open on port ${config.PORT}`);
   await connectDatabase();
-
-  await mongoose.connection.dropDatabase()
-
-
 })
