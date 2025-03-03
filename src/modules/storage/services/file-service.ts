900c12c6512b703be0e19ff727003a64fa9f79a2
DaviGGA <davigregorio.a@hotmail.com>
2025-03-03 18:06:34 -0300

    createFile

--- src/modules/storage/services/file-service.ts
+++ src/modules/storage/services/file-service.ts
@@ -1,6 +1,18 @@
 import { CreateFile } from "../@types/CreateFile";
+import { File } from "../models/StorageItem";
+import { pathHandler } from "../utils/path-handler";
 
 async function createFile({name, path, file}: CreateFile) {
+
+  const fileName = name ?? file.originalname
+
+  return await File.create({
+    name: fileName,
+    path: pathHandler.joinPath(fileName, path ?? null),
+    size: file.size,
+    mimetype: file.mimetype
+  })
+
 }
 
 
